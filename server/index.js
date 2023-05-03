7a5bc22295f527f0babf7596cd9c9d2039de2a92
Anand Pap <anandpap@live.com>
2023-05-03 02:18:00 +0200

    asd

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "/vercel.app$/" }));
 
 app.use(router);
 
