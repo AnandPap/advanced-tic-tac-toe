09189100598125873e94b2d66714d57c6686c410
Anand Pap <anandpap@live.com>
2025-02-25 02:23:40 +0100

    Dodan naziv baze.

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 const config = {
   port: process.env.PORT || 5000,
-  mongo: process.env.MONGO || "mongodb://127.0.0.1:27017",
+  mongo: process.env.MONGO || "mongodb://127.0.0.1:27017/Tic-tac-toe",
 };
 
 export default config;
